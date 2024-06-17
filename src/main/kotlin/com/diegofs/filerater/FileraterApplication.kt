6922146b9d1f28b1c60348c5e325e712125d1f98
Diego <diegofdezsrz12@gmail.com>
2024-06-17 17:44:52 +0200

    Initial project structure

---
+++ src/main/kotlin/com/diegofs/filerater/FileraterApplication.kt
@@ -0,0 +1,11 @@
+package com.diegofs.filerater
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FileraterApplication
+
+fun main(args: Array<String>) {
+	runApplication<FileraterApplication>(*args)
+}
---
+++ src/test/kotlin/com/diegofs/filerater/FileraterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.diegofs.filerater
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FileraterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
